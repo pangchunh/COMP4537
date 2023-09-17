c44a068e1220e4069f03155ff1dd940920748be3
Chun <peterpang1103@gmail.com>
2023-09-17 01:30:41 -0700

    remake directory

--- COMP4537/labs/1/js/util.js
+++
@@ -1,6 +0,0 @@
-export default function getLocalSotrageItemAsString(key){
-  const savedItem = localStorage.getItem(key)
-  if (savedItem){
-    return JSON.parse(savedItem)
-  } else return null
-}
