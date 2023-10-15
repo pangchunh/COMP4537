142d3fb7657e31e61ebc8dc471c55c5f4f56764e
Chun <peterpang1103@gmail.com>
2023-10-15 09:24:25 -0700

    fix behavior of insert route

--- labs/5/backend/app.js
+++ labs/5/backend/app.js
@@ -11,86 +11,90 @@ const createTableScript = `CREATE TABLE IF NOT EXISTS patient (
 
 db.createTable(createTableScript)
 
-//testing db functionaliy
-async function readDb(){
-  const result = await db.query(`SELECT * FROM patient`)
-  console.log(result.rows[0])
-}
 
-const testQuery = `SELECT * FROM patient`
-
-
-async function testOtherOps(){
-  try{
-    const result = await db.query(`DELETE FROM patient WHERE patientid = 1`)
-  } catch (e){
-    console.log(`db error ${e}`)
+async function testOtherOps() {
+  try {
+    return await db.query(`DELETE FROM patient WHERE patientid = 1`)
+  } catch (e) {
+    console.log(`Should have catch error. db error ${e}`)
   }
 }
 
-async function executeQuery(query){
-  try{
+async function executeQuery(query) {
+  try {
     const result = await db.query(query)
     console.log(result.rows)
     return result.rows
 
-  }catch(err){
+  } catch (err) {
     return err
   }
 }
 
 
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const parsedUrl = url.parse(req.url, true);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-  if (parsedUrl.pathname === '/query'){
-    console.log('query received')
-    let reqBody = ""
-    req.on('data', (chunk) => {
-      reqBody += chunk
-    })
-    try{
-      const postBody = JSON.parse(reqBody)
+  if (parsedUrl.pathname.startsWith === '/query') {
+    res.setHeader('Content-Type', 'application/json')
+    if (req.method === 'GET') {
+      try {
+        //get the query and execute it
+        const query = req.query.dbquery
+        const result = await executeQuery(query)
 
-      const result = executeQuery(postBody)
-    }catch(err){
-      console.log(err)
 
+      } catch (err) {
+
+      }
+
+
+    } else if (req.method === 'POST') {
+      let reqBody = ""
+      req.on('data', (chunk) => {
+        reqBody += chunk
+      })
+      try {
+        const postBody = JSON.parse(reqBody)
+
+        const result = executeQuery(postBody)
+      } catch (err) {
+        console.log(err)
+
+      }
+      //parse body and execute body
+    } else {
+      //method not allowed.
     }
 
 
-  }else if (parsedUrl.pathname === '/insert'){
-    console.log('insert route being called')
-    let reqBody = ""
-    req.on('data', (chunk) => {
-      reqBody += chunk
-    })
-    try{
-      req.on('end', async () => {
-        res.setHeader('Content-Type', 'application/json')
-        const postBody = JSON.parse(reqBody)
-        const result = await executeQuery(postBody)
-        res.statusCode = 200
-        res.end(JSON.stringify({result}))
-      })
+  } else if (parsedUrl.pathname === '/insert') {
+    res.setHeader('Content-Type', 'application/json')
+    const query = `INSERT INTO patient(name, dateofbirth)
+    values
+    ('Sara Brown', '1901-01-01'),
+    ('John Smith', '1941-01-01'),
+    ('Jack Ma', '1961-01-30'),
+    ('Elon Musk', '1999-01-01')`
 
-      
-    }catch(err){
+    try {
+      const result = await executeQuery(query)
+      res.statusCode = 200
+      res.end(JSON.stringify({ result }))
+
+    } catch (err) {
       console.log(err)
       res.statusCode = 500
-      res.end(JSON.stringify({err}))
-
+      res.end(JSON.stringify({ err }))
     }
-
-  }else if (req.method === 'OPTIONS') {
+  } else if (req.method === 'OPTIONS') {
     res.statusCode = 200;
     res.end()
   }
-
 })
 
 const PORT = process.env.PORT || 3000
